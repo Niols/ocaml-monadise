00f49a91af571afc08894ff1fb3af320f6b8353c
Niols <niols@niols.fr>
2025-11-04 15:54:53 +0100

    Enter Topiary

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    topiary = {
+      url = "github:tweag/topiary";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -13,11 +17,20 @@
       self,
       nixpkgs,
       git-hooks,
+      topiary,
     }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       opkgs = pkgs.ocamlPackages;
+
+      inherit (topiary.lib.${system}) gitHookBinFor gitHookFor;
+      myTopiaryConfig = {
+        includeLanguages = [
+          "ocaml"
+          "ocaml_interface"
+        ];
+      };
 
     in
     {
@@ -33,7 +46,9 @@
       devShells.${system}.default = pkgs.mkShell {
         inputsFrom = [ self.packages.${system}.default ];
         inherit (self.checks.${system}.git-hooks) shellHook;
-        buildInputs = self.checks.${system}.git-hooks.enabledPackages;
+        buildInputs = self.checks.${system}.git-hooks.enabledPackages ++ [
+          (gitHookBinFor myTopiaryConfig)
+        ];
       };
 
       checks.${system}.git-hooks = git-hooks.lib.${system}.run {
@@ -43,6 +58,9 @@
           dune-fmt.enable = true;
           dune-opam-sync.enable = true;
           nixfmt-rfc-style.enable = true;
+          topiary-latest = gitHookFor myTopiaryConfig // {
+            enable = true;
+          };
         };
       };
     };
@@ -50,9 +68,11 @@
   nixConfig = {
     extra-trusted-substituters = [
       "https://pre-commit-hooks.cachix.org/"
+      "https://tweag-topiary.cachix.org/"
     ];
     extra-trusted-public-keys = [
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+      "tweag-topiary.cachix.org-1:8TKqya43LAfj4qNHnljLpuBnxAY/YwEBfzo3kzXxNY0="
     ];
   };
 }
