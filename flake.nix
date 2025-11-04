da862d0961fc14131978de76ebbc11e579b47fcf
Niols <niols@niols.fr>
2025-11-04 15:53:16 +0100

    Enter Git hooks

--- flake.nix
+++ flake.nix
@@ -1,12 +1,24 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      git-hooks,
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       opkgs = pkgs.ocamlPackages;
+
     in
     {
       packages.${system}.default = opkgs.buildDunePackage {
@@ -20,6 +32,27 @@
 
       devShells.${system}.default = pkgs.mkShell {
         inputsFrom = [ self.packages.${system}.default ];
+        inherit (self.checks.${system}.git-hooks) shellHook;
+        buildInputs = self.checks.${system}.git-hooks.enabledPackages;
+      };
+
+      checks.${system}.git-hooks = git-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          deadnix.enable = true;
+          dune-fmt.enable = true;
+          dune-opam-sync.enable = true;
+          nixfmt-rfc-style.enable = true;
+        };
       };
     };
+
+  nixConfig = {
+    extra-trusted-substituters = [
+      "https://pre-commit-hooks.cachix.org/"
+    ];
+    extra-trusted-public-keys = [
+      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+    ];
+  };
 }
